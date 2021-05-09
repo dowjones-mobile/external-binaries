1a6bde07f0ac45342f60d0a12e00eae300a6a85a
Florian Harr <florian.harr@wsj.com>
2021-05-09 18:51:14 -0400

    Adding Package.swift

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "external-binaries",
+    products: [
+        .library(name: "GoogleMobileAds", targets: ["GoogleMobileAds"]),
+        .library(name: "GoogleAppMeasurement", targets: ["GoogleAppMeasurement"]),
+        .library(name: "GoogleUtilities", targets: ["GoogleUtilities"]),
+        .library(name: "PromisesObjC", targets: ["PromisesObjC"]),
+        .library(name: "UserMessagingPlatform", targets: ["UserMessagingPlatform"]),
+        .library(name: "nanopb", targets: ["nanopb"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "GoogleMobileAds",
+            path: "GoogleMobileAds/7.69.0/GoogleMobileAds.xcframework"
+        ),
+        .binaryTarget(
+            name: "GoogleAppMeasurement",
+            path: "GoogleMobileAds/7.69.0/GoogleAppMeasurement.xcframework"
+        ),
+        .binaryTarget(
+            name: "GoogleUtilities",
+            path: "GoogleMobileAds/7.69.0/GoogleUtilities.xcframework"
+        ),
+        .binaryTarget(
+            name: "PromisesObjC",
+            path: "GoogleMobileAds/7.69.0/PromisesObjC.xcframework"
+        ),
+        .binaryTarget(
+            name: "UserMessagingPlatform",
+            path: "GoogleMobileAds/7.69.0/UserMessagingPlatform.xcframework"
+        ),
+        .binaryTarget(
+            name: "nanopb",
+            path: "GoogleMobileAds/7.69.0/nanopb.xcframework"
+        ),
+    ]
+)
